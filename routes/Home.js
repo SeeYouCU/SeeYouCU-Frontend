cff523b672c7684b9e3d4fbd4bb1755f971160e9
sunnypawat <sunnypawat@gmail.com>
2023-04-02 22:57:56 +0700

    setting up the profile setup page

--- routes/Home.js
+++ routes/Home.js
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import {View, Text} from 'react-native';
+import ProfileSetup from './ProfileSetup.js';
 
 export default function Match({navigation}) {
   return (
     <View>
-      <Text>Match</Text>
+       <ProfileSetup/>
     </View>
   );
 }
