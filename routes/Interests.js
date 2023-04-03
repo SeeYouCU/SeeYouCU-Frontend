b1de187ea58f6b9f04a7183552b1d39e70b28fc3
Penpim J <penpimjir@gmail.com>
2023-04-03 16:31:57 +0700

    Insert interest icons with draft styles

--- routes/Interests.js
+++ routes/Interests.js
@@ -7,6 +7,7 @@ import {
   Dimensions,
   StyleSheet,
   ImageBackground,
+  Image,
 } from 'react-native';
 import {useForm, Controller} from 'react-hook-form';
 
@@ -42,28 +43,14 @@ export default function Interests({navigation}) {
       title: 'Anime',
       img: '',
     },
-    {
-      key: 'Badminton',
-      title: 'Badminton',
-      img: '',
-    },
-    {
-      key: 'Basketball',
-      title: 'Basketball',
-      img: '',
-    },
-    {
-      key: 'Baking',
-      title: 'Baking',
-      img: '',
-    },
   ];
 
   const intrList = () => {
     return intrArray.map(element => {
       return (
-        <View key={element.key}>
-          <Text>{element.title}</Text>
+        <View key={element.key} style={styles.interest}>
+          <Image source={require('../public/anime.png')} />
+          <Text style={styles.h3}>{element.title}</Text>
         </View>
       );
     });
@@ -75,13 +62,12 @@ export default function Interests({navigation}) {
       style={styles.container}>
       <View>
         {/* <Button title="Back" onPress={() => navigation.goBack()} /> */}
-        <Text style={styles.title}>Pick Your Top 5 Interests</Text>
-        {/* search bar */}
-        <View>
-          <Text>Your Interests</Text>
-          {intrList()}
-        </View>
-
+        <Text style={styles.h1}>Pick Your Top 5 Interests</Text>
+        <Text>(Will be search bar)</Text>
+        <View style={styles.content}>{intrList()}</View>
+        <View style={styles.content}>{intrList()}</View>
+        <View style={styles.content}>{intrList()}</View>
+        <View style={styles.content}>{intrList()}</View>
         <Controller
           control={control}
           rules={{
@@ -101,17 +87,33 @@ export default function Interests({navigation}) {
 }
 
 const styles = StyleSheet.create({
-  title: {
-    paddingTop: 0.05 * Dimensions.get('window').width,
-    backgroundColor: 'transparent',
-    fontSize: 24,
-    color: '#155E6D',
-    textAlign: 'center',
-  },
   container: {
     resizeMode: 'cover',
     height: Dimensions.get('window').height,
     width: Dimensions.get('window').width,
     flex: 1,
+  },
+  h1: {
+    paddingTop: 0.08 * Dimensions.get('window').width,
+    fontSize: 24,
+    color: '#155E6D',
+    textAlign: 'center',
+    fontWeight: 700,
+  },
+  content: {
+    flexDirection: 'row',
+  },
+  h3: {
+    color: '#155E6D',
+    fontSize: 16,
+    fontWeight: 500,
+    textAlign: 'center',
+    paddingTop: 0.01 * Dimensions.get('window').width,
+  },
+  interest: {
+    alignItems: 'center',
+    width: 0.3 * Dimensions.get('window').width,
+    margin: 0.01 * Dimensions.get('window').width,
+    padding: 0.01 * Dimensions.get('window').width,
   },
 });
