c794f46c1127818fb17f2416515c0847adba8e8a
leilaigl <leilasigles@gmail.com>
2023-04-14 23:24:24 +0700

    wip: handle select

--- routes/Interests.js
+++ routes/Interests.js
@@ -2,64 +2,153 @@ import React, {useState} from 'react';
 import {
   Text,
   View,
-  TextInput,
-  Button,
   Dimensions,
   StyleSheet,
   ImageBackground,
   Image,
   TouchableOpacity,
+  ScrollView,
 } from 'react-native';
-import {useForm} from 'react-hook-form';
+import Input from '../components/Input';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export default function Interests({navigation}) {
-  const {
-    control,
-    handleSubmit,
-    formState: {errors},
-  } = useForm({
-  const {
-    control,
-    handleSubmit,
-    formState: {errors},
-  } = useForm({
-    defaultValues: {
-      interests: '',
-    },
-      interests: '',
-    },
-  });
+  const [getInterests, setInterests] = React.useState([]);
 
-  const onSubmit = data => {
-    console.log(data);
-    navigation.navigate('Match');
+  const onSubmit = () => {
+    console.log(getInterests);
+    // navigation.navigate('Match');
   };
 
   const interests = [
     {
       key: 'AerobicDance',
+      src: 'https://www.stylecraze.com/wp-content/uploads/2015/01/04.jpg',
       title: 'Aerobic Dance',
       isSelected: false,
     },
     {
       key: 'Acting',
+      src: 'https://theatre.ua.edu/wp-content/uploads/2019/10/17-18-Vinegar-Tom-JH-1024x684.jpg',
       title: 'Acting',
       isSelected: false,
     },
     {
       key: 'Anime',
+      src: 'https://assets-prd.ignimgs.com/2022/08/17/top25animecharacters-blogroll-1660777571580.jpg',
+      title: 'Anime',
+      isSelected: false,
+    },
+    {
+      key: 'Badminton',
+      src: 'https://ss-i.thgim.com/public/incoming/wf966c/article66364426.ece/alternates/FREE_1200/GettyImages-1409229566.jpg',
+      title: 'Badminton',
+      isSelected: false,
+    },
+    {
+      key: 'Basketball',
+      src: 'https://cdn.nba.com/manage/2023/04/GettyImages-1239701619-scaled.jpg',
+      title: 'Basketball',
+      isSelected: false,
+    },
+    {
+      key: 'Baking',
+      src: 'https://static.toiimg.com/photo/75536288.cms',
+      title: 'Anime',
+      isSelected: false,
+    },
+    {
+      key: 'AerobicDance1',
+      src: 'https://www.stylecraze.com/wp-content/uploads/2015/01/04.jpg',
+      title: 'Aerobic Dance',
+      isSelected: false,
+    },
+    {
+      key: 'Acting1',
+      src: 'https://theatre.ua.edu/wp-content/uploads/2019/10/17-18-Vinegar-Tom-JH-1024x684.jpg',
+      title: 'Acting',
+      isSelected: false,
+    },
+    {
+      key: 'Anime1',
+      src: 'https://assets-prd.ignimgs.com/2022/08/17/top25animecharacters-blogroll-1660777571580.jpg',
+      title: 'Anime',
+      isSelected: false,
+    },
+    {
+      key: 'Badminton1',
+      src: 'https://ss-i.thgim.com/public/incoming/wf966c/article66364426.ece/alternates/FREE_1200/GettyImages-1409229566.jpg',
+      title: 'Badminton',
+      isSelected: false,
+    },
+    {
+      key: 'Basketball1',
+      src: 'https://cdn.nba.com/manage/2023/04/GettyImages-1239701619-scaled.jpg',
+      title: 'Basketball',
+      isSelected: false,
+    },
+    {
+      key: 'Baking1',
+      src: 'https://static.toiimg.com/photo/75536288.cms',
+      title: 'Anime',
+      isSelected: false,
+    },
+    {
+      key: 'AerobicDance2',
+      src: 'https://www.stylecraze.com/wp-content/uploads/2015/01/04.jpg',
+      title: 'Aerobic Dance',
+      isSelected: false,
+    },
+    {
+      key: 'Acting2',
+      src: 'https://theatre.ua.edu/wp-content/uploads/2019/10/17-18-Vinegar-Tom-JH-1024x684.jpg',
+      title: 'Acting',
+      isSelected: false,
+    },
+    {
+      key: 'Anime2',
+      src: 'https://assets-prd.ignimgs.com/2022/08/17/top25animecharacters-blogroll-1660777571580.jpg',
+      title: 'Anime',
+      isSelected: false,
+    },
+    {
+      key: 'Badminton2',
+      src: 'https://ss-i.thgim.com/public/incoming/wf966c/article66364426.ece/alternates/FREE_1200/GettyImages-1409229566.jpg',
+      title: 'Badminton',
+      isSelected: false,
+    },
+    {
+      key: 'Basketball2',
+      src: 'https://cdn.nba.com/manage/2023/04/GettyImages-1239701619-scaled.jpg',
+      title: 'Basketball',
+      isSelected: false,
+    },
+    {
+      key: 'Baking2',
+      src: 'https://static.toiimg.com/photo/75536288.cms',
       title: 'Anime',
       isSelected: false,
     },
   ];
 
-  const [selectedIntr, setSelectedIntr] = useState([]);
+  const [selectedInterest, setSelectedInterest] = useState([]);
+
+  const handleSelectPill = (id, select) => {
+    const temp = selectedInterest;
+    if (select == true) setSelectedInterest({data: [...temp, id]});
+    if (select == false)
+      setSelectedInterest({
+        data: temp.filter(item => JSON.stringify(item) != JSON.stringify(id)),
+      });
+  };
+
+  console.log(selectedInterest);
 
   const toggleSelection = key => {
     const index = interests.findIndex(item => item.key === key);
     const newInterests = [...interests];
     newInterests[index].isSelected = !newInterests[index].isSelected;
-    setSelectedIntr(
+    setSelectedInterest(
       newInterests.filter(item => item.isSelected).map(item => item.key),
     );
     if (!newInterests[index].isSelected) {
@@ -67,16 +156,30 @@ export default function Interests({navigation}) {
     }
   };
 
-  const intrList = () => {
+  const interestList = () => {
     return interests.map((item, index) => {
-      const intrIcon = selectedIntr.includes(item.key)
-        ? require('../public/checked.png')
-        : require('../public/anime.png');
+      const imageBlur = selectedInterest.includes(item.key) ? 30 : 0;
       return (
         <TouchableOpacity key={index} onPress={() => toggleSelection(item.key)}>
-          <View style={styles.interest}>
-            <Image source={intrIcon} />
-            <Text style={styles.h3}>{item.title}</Text>
+          <View style={styles.interestItem}>
+            <View style={styles.itemImgFrame}>
+              <Image
+                source={{
+                  uri: item.src,
+                }}
+                blurRadius={imageBlur}
+                style={styles.itemImg}
+              />
+              {selectedInterest.includes(item.key) ? (
+                <Icon
+                  name="checkmark"
+                  size={32}
+                  color="white"
+                  style={{position: 'absolute', zIndex: 0}}
+                />
+              ) : null}
+            </View>
+            <Text style={styles.itemName}>{item.title}</Text>
           </View>
         </TouchableOpacity>
       );
@@ -87,17 +190,17 @@ export default function Interests({navigation}) {
     <ImageBackground
       source={require('../public/bg.png')}
       style={styles.container}>
-      {/* <Button title="Back" onPress={() => navigation.goBack()} /> */}
-      <Text style={styles.h1}>Pick Your Top 5 Interests</Text>
-      <Text style={styles.search}>Search</Text>
-      <View style={styles.interestPan}>
-        <View style={styles.row}>{intrList()}</View>
-        <View style={styles.row}>{intrList()}</View>
-        <View style={styles.row}>{intrList()}</View>
-        <View style={styles.row}>{intrList()}</View>
-      </View>
-      <TouchableOpacity onPress={handleSubmit(onSubmit)}>
-        <Text style={styles.btn}>Next {'>'}</Text>
+      <Text style={styles.title}>Pick Your Top 5 Interests</Text>
+      <Input
+        isSearch="true"
+        placeholder="Search"
+        style={{marginTop: '5%', marginBottom: '5%'}}
+      />
+      <ScrollView fadingEdgeLength={100}>
+        <View style={styles.interestMap}>{interestList()}</View>
+      </ScrollView>
+      <TouchableOpacity onPress={handleSelectPill}>
+        <Text style={styles.nextBtn}>Next {'>'}</Text>
       </TouchableOpacity>
     </ImageBackground>
   );
@@ -109,11 +212,31 @@ const styles = StyleSheet.create({
     height: Dimensions.get('window').height,
     width: Dimensions.get('window').width,
     flex: 1,
+    padding: '5%',
   },
-  interestPan: {
+  itemImgFrame: {
+    display: 'flex',
     alignItems: 'center',
+    justifyContent: 'center',
+    overflow: 'hidden',
+    height: 90,
+    width: 90,
+    borderRadius: 70,
+    margin: '3%',
   },
-  h1: {
+  itemImg: {
+    resizeMode: 'cover',
+    height: '100%',
+    width: '100%',
+  },
+  interestMap: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  title: {
     paddingTop: 0.08 * Dimensions.get('window').width,
     fontSize: 24,
     color: '#155E6D',
@@ -127,27 +250,26 @@ const styles = StyleSheet.create({
     paddingLeft: 0.04 * Dimensions.get('window').width,
     padding: 0.01 * Dimensions.get('window').width,
   },
-  row: {
-    flexDirection: 'row',
-  },
-  h3: {
+  itemName: {
     color: '#155E6D',
     fontSize: 16,
     fontWeight: 500,
     textAlign: 'center',
     paddingTop: 0.01 * Dimensions.get('window').width,
   },
-  interest: {
-    alignItems: 'center',
+  interestItem: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
     width: 0.3 * Dimensions.get('window').width,
-    margin: 0.01 * Dimensions.get('window').width,
-    padding: 0.01 * Dimensions.get('window').width,
+    marginBottom: '20%',
   },
-  btn: {
+  nextBtn: {
     textAlign: 'right',
     paddingRight: 0.05 * Dimensions.get('window').width,
     color: '#155E6D',
     fontSize: 20,
     fontWeight: 600,
+    marginTop: '5%',
   },
 });
