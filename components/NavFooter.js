8c96319ed7a74de4ac74ab3361506c4c2d9b799b
Penpim J <penpimjir@gmail.com>
2023-04-16 19:41:21 +0700

    Add icons to the nav and add styles

--- components/NavFooter.js
+++ components/NavFooter.js
@@ -1,21 +1,30 @@
 import React from 'react';
-import {StyleSheet, View, Dimensions, Text} from 'react-native';
+import {StyleSheet, View, Dimensions, Image} from 'react-native';
 
 const NavFooter = props => {
   return (
     <View style={styles.container}>
-      <Text>NavFooter</Text>
+      <Image source={require('../public/handshake.png')} style={styles.icon} />
+      <Image source={require('../public/event.png')} style={styles.icon} />
+      <Image source={require('../public/item.png')} style={styles.icon} />
+      <Image source={require('../public/qr.png')} style={styles.icon} />
+      <Image source={require('../public/settings.png')} style={styles.icon} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#000000',
-    width: 0.9 * Dimensions.get('window').width,
+    flexDirection: 'row',
+    backgroundColor: '#ffffff',
+    width: 0.9 * Dimensions.get('window').width, // maybe a bit weird because idk how to fix padding in Match page
     height: 0.08 * Dimensions.get('window').height,
     borderRadius: 24,
     alignItems: 'center',
+    justifyContent: 'center',
+  },
+  icon: {
+    margin: 0.06 * Dimensions.get('window').width,
   },
 });
 
