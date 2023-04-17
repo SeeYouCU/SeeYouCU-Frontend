a0bfc00d05d640f707c6541ba0ceacb5303050f2
Penpim J <penpimjir@gmail.com>
2023-04-17 19:06:11 +0700

    Edit useState (dummy working for toggle icons)

--- components/NavFooter.js
+++ components/NavFooter.js
@@ -8,15 +8,35 @@ import {
 } from 'react-native';
 
 const NavFooter = props => {
-  const [icons, setIcons] = useState([
-    {source: require('../public/handshake.png'), key: 0},
-    {source: require('../public/event.png'), key: 1},
-    {source: require('../public/item.png'), key: 2},
-    {source: require('../public/qr.png'), key: 3},
-    {source: require('../public/settings.png'), key: 4},
-  ]);
+  const icons = [
+    {
+      key: 0,
+      source: require('../public/handshake.png'),
+      isSelected: false,
+    },
+    {
+      key: 1,
+      source: require('../public/event.png'),
+      isSelected: false,
+    },
+    {
+      key: 2,
+      source: require('../public/item.png'),
+      isSelected: false,
+    },
+    {
+      key: 3,
+      source: require('../public/qr.png'),
+      isSelected: false,
+    },
+    {
+      key: 4,
+      source: require('../public/settings.png'),
+      isSelected: false,
+    },
+  ];
 
-  const iconSelected = [
+  const iconSelectedSrc = [
     require('../public/handshake1.png'),
     require('../public/event1.png'),
     require('../public/item1.png'),
@@ -24,34 +44,35 @@ const NavFooter = props => {
     require('../public/settings1.png'),
   ];
 
+  const [selectedIcon, setSelectedIcon] = useState([]);
+
+  const handleIconPress = key => {
+    const newIcons = [...icons];
+    newIcons[key].isSelected = !newIcons[key].isSelected;
+    setSelectedIcon(
+      newIcons.filter(icon => icon.isSelected).map(icon => icon.key),
+    );
+    if (!newIcons[key].isSelected) {
+      newIcons[key].isSelected = false;
+    }
+  };
+
   const iconList = () => {
     return icons.map(icon => {
+      const iconSrc = !selectedIcon.includes(icon.key)
+        ? icon.source
+        : iconSelectedSrc[icon.key];
       return (
         <TouchableOpacity
           key={icon.key}
           onPress={() => handleIconPress(icon.key)}>
-          <Image source={icon.source} style={styles.icon} />
+          <Image source={iconSrc} style={styles.icon} />
         </TouchableOpacity>
       );
     });
   };
 
-  const handleIconPress = key => {
-    const newIcons = [...icons];
-    newIcons[key].source = iconSelected[key];
-    setIcons(newIcons);
-  };
-
-  return (
-    <View style={styles.container}>
-      {/* <Image source={require('../public/handshake.png')} style={styles.icon} />
-      <Image source={require('../public/event.png')} style={styles.icon} />
-      <Image source={require('../public/item.png')} style={styles.icon} />
-      <Image source={require('../public/qr.png')} style={styles.icon} />
-      <Image source={require('../public/settings.png')} style={styles.icon} /> */}
-      {iconList()}
-    </View>
-  );
+  return <View style={styles.container}>{iconList()}</View>;
 };
 
 const styles = StyleSheet.create({
