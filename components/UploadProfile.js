d31d47671866a54a140a7d7d37cc04ee12bdb7f0
sunnypawat <sunnypawat@gmail.com>
2023-04-30 15:27:15 +0700

    handle photo upload

--- components/UploadProfile.js
+++ components/UploadProfile.js
@@ -1,9 +1,22 @@
 import * as React from 'react';
-import {StyleSheet, View, Image} from 'react-native';
+import {StyleSheet, View, Image, Button, TouchableHighlight} from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import LinearGradient from 'react-native-linear-gradient';
+import { launchImageLibrary } from 'react-native-image-picker';
 
 const UploadProfile = props => {
+  const [photo, setPhoto] = React.useState(null);
+  const [clicked, setClicked] = React.useState(false);
+
+  const handleChoosePhoto = () => {
+    launchImageLibrary({noData: true}, (response) => {
+      if (response) {
+        setClicked(true);
+        setPhoto(response);
+        console.log(response);
+      }
+    });
+  };
   //TODO: take image, upload image
   return (
     <LinearGradient
@@ -11,22 +24,16 @@ const UploadProfile = props => {
       start={{x: 3, y: 0}}
       end={{x: 0, y: 3}}
       style={styles.linearGradient}>
-      <View style={styles.frame}>
-        {props.isClicked == false ? (
-          <Icon
-            name="camera"
-            size={40}
-            color="#155E6D"
-            style={{position: 'absolute', zIndex: 0}}
-          />
-        ) : (
-          <Image
-            source={{
-              uri: props.src,
-            }}
-            style={{resizeMode: 'cover', width: '100%', height: '100%'}}
-          />
-        )}
+      <View style={styles.frame} onPress={handleChoosePhoto}>
+          <TouchableHighlight onPress={handleChoosePhoto}>
+          <View>
+            <Icon
+              name="camera"
+              size={40}
+              color="#155E6D"
+            />
+          </View>
+        </TouchableHighlight>
       </View>
     </LinearGradient>
   );
