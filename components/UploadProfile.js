edc81333e4eb918acd8fc43998a6aae9db7fe9cf
sunnypawat <sunnypawat@gmail.com>
2023-05-04 08:09:12 +0700

    add permissions and more on styling

--- components/UploadProfile.js
+++ components/UploadProfile.js
@@ -19,23 +19,38 @@ const UploadProfile = props => {
   };
   //TODO: take image, upload image
   return (
+    <TouchableHighlight onPress={handleChoosePhoto}>
     <LinearGradient
       colors={['#2ba0ad', '#f0f7f8', '#2ba0ad']}
       start={{x: 3, y: 0}}
       end={{x: 0, y: 3}}
-      style={styles.linearGradient}>
-      <View style={styles.frame} onPress={handleChoosePhoto}>
-          <TouchableHighlight onPress={handleChoosePhoto}>
-          <View>
-            <Icon
-              name="camera"
-              size={40}
-              color="#155E6D"
-            />
-          </View>
-        </TouchableHighlight>
+      style={styles.linearGradient}
+    >
+      <View style={styles.frame}>
+          { !photo
+           ? <TouchableHighlight>
+              <View>
+                <Icon
+                  name="camera"
+                  size={40}
+                  color="#155E6D"
+                />
+              </View>
+            </TouchableHighlight>
+          : <TouchableHighlight>
+              <View>
+              <Image
+                source={{
+                  uri: photo.uri,
+                }}
+                style={{resizeMode: 'cover', width: '100%', height: '100%'}}
+              />
+              </View>
+            </TouchableHighlight>
+          }
       </View>
     </LinearGradient>
+    </TouchableHighlight>
   );
 };
 
